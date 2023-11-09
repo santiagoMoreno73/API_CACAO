7be15d5738c65f44357923e4358bddccf91943c2
santiagoMoreno73 <santimor2009@hotmail.com>
2023-11-09 00:22:23 -0500

    added add and remove

--- src/modules/customers/controller.js
+++ src/modules/customers/controller.js
@@ -10,7 +10,17 @@ function one(id) {
   return db.one(TABLE, id);
 }
 
+function add(body) {
+  return db.add(TABLE, body);
+}
+
+function remove(body) {
+  return db.remove(TABLE, body);
+}
+
 module.exports = {
   all,
   one,
+  add,
+  remove,
 };
