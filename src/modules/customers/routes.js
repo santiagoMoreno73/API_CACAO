cbf6d65033f453b6fcf2483b3f13b656f1edd6e7
santiagoMoreno73 <santimor2009@hotmail.com>
2023-11-09 00:22:40 -0500

    added error structure to routes

--- src/modules/customers/routes.js
+++ src/modules/customers/routes.js
@@ -4,23 +4,52 @@ const answers = require("../../red/answers");
 const controller = require("./controller");
 const router = express.Router();
 
-router.get("/", async function (req, res) {
+router.get("/", all);
+router.get("/:id", one);
+router.post("/", add);
+router.put("/", remove);
+
+async function all(req, res, next) {
   try {
     const items = await controller.all();
     answers.success(req, res, items, 200);
   } catch (error) {
-    response.error(req, res, error, 500);
+    next(error);
   }
-});
+}
 
-router.get("/:id", async function (req, res) {
+async function one(req, res, next) {
   try {
     const items = await controller.one(req.params.id);
     answers.success(req, res, items, 200);
   } catch (error) {
-    response.error(req, res, error, 500);
+    next(error);
   }
-});
+}
+
+async function add(req, res, next) {
+  try {
+    const items = await controller.add(req.body);
+    if (req.body.id == 0) {
+      message = "The item was saved successfully";
+    } else {
+      message = "The item was updated successfully";
+    }
+
+    answers.success(req, res, message, 201);
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function remove(req, res, next) {
+  try {
+    const items = await controller.remove(req.body);
+    answers.success(req, res, "Item deleted successfully", 200);
+  } catch (error) {
+    next(error);
+  }
+}
 
 // export
 module.exports = router;
