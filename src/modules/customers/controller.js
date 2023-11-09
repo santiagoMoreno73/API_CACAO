44ffd43b37e6e97f1c873568a8f27051a74b7ce2
santiagoMoreno73 <santimor2009@hotmail.com>
2023-11-09 13:38:12 -0500

    refactor controller

--- src/modules/customers/controller.js
+++ src/modules/customers/controller.js
@@ -1,26 +1,61 @@
-const db = require("../../db/mysql");
-
 const TABLE = "user";
+const auth = require("../auth");
 
-function all() {
-  return db.all(TABLE);
-}
+module.exports = function (db_injected) {
+  let db = db_injected;
 
-function one(id) {
-  return db.one(TABLE, id);
-}
+  if (!db) {
+    db = require("../../db/mysql");
+  }
 
-function add(body) {
-  return db.add(TABLE, body);
-}
+  function all() {
+    return db.all(TABLE);
+  }
 
-function remove(body) {
-  return db.remove(TABLE, body);
-}
+  function one(id) {
+    return db.one(TABLE, id);
+  }
 
-module.exports = {
-  all,
-  one,
-  add,
-  remove,
+  async function add(body) {
+    const user = {
+      id: body.id,
+      name: body.name,
+      email: body.email,
+      address: body.address,
+      active: body.active,
+    };
+
+    const response = await db.add(TABLE, user);
+    let insertId = 0;
+
+    if (body.id == 0) {
+      insertId = response.insertId;
+    } else {
+      insertId = body.id;
+    }
+
+    let response_two = "";
+    if (body.user || body.password) {
+      const user = {
+        id: insertId,
+        user: body.user,
+        password: body.password,
+      };
+
+      response_two = await auth.add(user);
+    }
+
+    return response_two;
+  }
+
+  function remove(body) {
+    return db.remove(TABLE, body);
+  }
+
+  return {
+    all,
+    one,
+    add,
+    remove,
+  };
 };
